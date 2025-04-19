90fc303764c37c66425cda5fc930be103dd9c215
TimiWho <76894401+TimiWho@users.noreply.github.com>
2025-04-19 16:45:31 +0200

    workon

--- esp8266_ota/esp8266_ota.ino
+++ esp8266_ota/esp8266_ota.ino
@@ -1,174 +1,116 @@
+/*
+ * Basic Sketch for automatic update of firmware at start
+ * with firmware version control
+ *
+ * Renzo Mischianti <www.mischianti.org>
+ *
+ * https://mischianti.org
+ * https://mischianti.org/esp8266-self-ota-update-firmware-from-server-with-version-check-2/
+*/
+ 
+#include <Arduino.h>
+ 
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+ 
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
-#include <WiFiClientSecure.h>
-#include <CertStoreBearSSL.h>
-BearSSL::CertStore certStore;
-#include <time.h>
-
  
-const String FirmwareVer={"2.2"}; 
-#define URL_fw_Version "https://raw.githubusercontent.com//TimiWho/ESP8266_ESP32_SelfUpdate/blob/master/esp8266_ota/bin_version.txt"
-//#define URL_fw_Version "/programmer131/otaFiles/master/version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/TimiWho/ESP8266_ESP32_SelfUpdate/blob/master/esp8266_ota/fw.bin" // https://upgrade.timi.at/
-const char* host = "raw.githubusercontent.com";
-const int httpsPort = 443;
-
-// DigiCert High Assurance EV Root CA
-const char trustRoot[] PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
-MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
-d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
-QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
-MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
-b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
-9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
-CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
-nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
-43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
-T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
-gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
-BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
-TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
-DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
-hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
-06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
-PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
-YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
-CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
------END CERTIFICATE-----
-)EOF";
-X509List cert(trustRoot);
-
-
-extern const unsigned char caCert[] PROGMEM;
-extern const unsigned int caCertLen;
-
-const char* ssid = "Timi_Home";
-const char* password = "timi397home_";
-
-void setClock() {
-   // Set time via NTP, as required for x.509 validation
-  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
-  Serial.print("Waiting for NTP time sync: ");
-  time_t now = time(nullptr);
-  while (now < 8 * 3600 * 2) {
-    delay(500);
-    Serial.print(".");
-    now = time(nullptr);
-  }
-
-  Serial.println("");
-  struct tm timeinfo;
-  gmtime_r(&now, &timeinfo);
-  Serial.print("Current time: ");
-  Serial.print(asctime(&timeinfo));
-
+#ifndef APSSID
+#define APSSID "Timi_Home"
+#define APPSK  "timi397home_"
+#endif
+ 
+ESP8266WiFiMulti WiFiMulti;
+ 
+#define FIRMWARE_VERSION "0.3"
+ 
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
 }
-  
-void FirmwareUpdate()
-{  
-  WiFiClientSecure client;
-  client.setTrustAnchors(&cert);
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("Connection failed");
-    return;
+ 
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+ 
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+ 
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+//   Serial.setDebugOutput(true);
+ 
+  Serial.println();
+  Serial.println();
+  Serial.println();
+ 
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
   }
-  client.print(String("GET ") + URL_fw_Version + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      //Serial.println("Headers received");
-      break;
-    }
-  }
-  String payload = client.readStringUntil('\n');
-
-  payload.trim();
-  if(payload.equals(FirmwareVer) )
-  {   
-     Serial.println("Device already on latest firmware version"); 
-  }
-  else
-  {
-    Serial.println("New firmware detected");
-    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW); 
-    t_httpUpdate_return ret = ESPhttpUpdate.update(client, URL_fw_Bin);
-        
+ 
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(APSSID, APPSK);
+ 
+  Serial.print(F("Firmware version "));
+  Serial.println(FIRMWARE_VERSION);
+  delay(2000);
+}
+ 
+ 
+void loop() {
+    // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+ 
+    WiFiClient client;
+ 
+    // The line below is optional. It can be used to blink the LED on the board during flashing
+    // The LED will be on during download of one buffer of data from the network. The LED will
+    // be off during writing that buffer to flash
+    // On a good connection the LED should flash regularly. On a bad connection the LED will be
+    // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
+    // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
+    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+ 
+    // Add optional callback notifiers
+    ESPhttpUpdate.onStart(update_started);
+    ESPhttpUpdate.onEnd(update_finished);
+    ESPhttpUpdate.onProgress(update_progress);
+    ESPhttpUpdate.onError(update_error);
+ 
+    ESPhttpUpdate.rebootOnUpdate(false); // remove automatic update
+ 
+    Serial.println(F("Update start now!"));
+ 
+     t_httpUpdate_return ret = ESPhttpUpdate.update(client, "https://upgrade.timi.at/fw.bin");
+    // Or:
+//    t_httpUpdate_return ret = ESPhttpUpdate.update(client, "192.168.1.70", 3000, "/update", FIRMWARE_VERSION);
+ 
     switch (ret) {
       case HTTP_UPDATE_FAILED:
         Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        Serial.println(F("Retry in 10secs!"));
+        delay(10000); // Wait 10secs
         break;
-
+ 
       case HTTP_UPDATE_NO_UPDATES:
         Serial.println("HTTP_UPDATE_NO_UPDATES");
+        Serial.println("Your code is up to date!");
+          delay(10000); // Wait 10secs
         break;
-
+ 
       case HTTP_UPDATE_OK:
         Serial.println("HTTP_UPDATE_OK");
+        delay(1000); // Wait a second and restart
+        ESP.restart();
         break;
-    } 
-  }
- }  
-void connect_wifi();
-unsigned long previousMillis_2 = 0;
-unsigned long previousMillis = 0;        // will store last time LED was updated
-const long interval = 60000;
-const long mini_interval = 3000;
- void repeatedCall(){
-    unsigned long currentMillis = millis();
-    if ((currentMillis - previousMillis) >= interval) 
-    {
-      // save the last time you blinked the LED
-      previousMillis = currentMillis;
-      setClock();
-      FirmwareUpdate();
     }
-
-    if ((currentMillis - previousMillis_2) >= mini_interval) {
-      static int idle_counter=0;
-      previousMillis_2 = currentMillis;    
-      Serial.print(" Active fw version:");
-      Serial.println(FirmwareVer);
-      Serial.print("Idle Loop....");
-      Serial.println(idle_counter++);
-     if(idle_counter%2==0)
-      digitalWrite(LED_BUILTIN, HIGH);
-     else 
-      digitalWrite(LED_BUILTIN, LOW);
-     if(WiFi.status() == !WL_CONNECTED) 
-          connect_wifi();
-   }
- }
-
-  
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("");
-  Serial.println("Start");
-  WiFi.mode(WIFI_STA);
-  connect_wifi();  
-  setClock();
-  pinMode(LED_BUILTIN, OUTPUT);
-  
-}
-void loop()
-{
-  repeatedCall();    
-}
-
-void connect_wifi()
-{
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print("O");
-  }                                   
-  Serial.println("Connected to WiFi");
+  }
 }
